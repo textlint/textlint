d3dade3e7e698c196ccce8594b4d4d6bd0b2fc05
azu <azuciao@gmail.com>
2014-12-27 21:51:22 +0900

    refactor(rule): move rule* to rule/

--- test/textlint-test.js
+++ test/textlint-test.js
@@ -2,7 +2,7 @@
 "use strict";
 var assert = require("power-assert");
 var textLint = require("../lib/textlint");
-var loadRules = require("../lib/load-rules");
+var loadRules = require("../lib/rule/load-rules");
 var rules = loadRules(__dirname + "/rules");
 describe("textlint-test", function () {
     beforeEach(function () {
