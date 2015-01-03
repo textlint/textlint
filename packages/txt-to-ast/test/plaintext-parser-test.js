aa2f8ed741aa06952c113f41e79b5746d4464782
azu <azuciao@gmail.com>
2015-01-04 00:38:31 +0900

    test(parser): add tests for parser

--- packages/txt-to-ast/test/plaintext-parser-test.js
+++ packages/txt-to-ast/test/plaintext-parser-test.js
@@ -4,28 +4,30 @@ var parse = require("../").parse;
 var Syntax = require("../").Syntax;
 var assert = require("power-assert");
 describe("plaintext-parser-test", function () {
-    it("should return AST", function () {
-        var text = "TEST text";
-        var ast = parse(text);
-        assert(typeof ast === "object");
-        // top type is always Document
-        assert.equal(ast.type, Syntax.Document);
-        assert(ast.loc, Syntax.Document);
-        // should has children
-        assert(ast.children.length > 0);
+    context("Document", function () {
+        it("should return AST", function () {
+            var text = "text";
+            var ast = parse(text);
+            assert(typeof ast === "object");
+            // top type is always Document
+            assert.equal(ast.type, Syntax.Document);
+            assert.deepEqual(ast.loc, {
+                start: {
+                    line: 1,
+                    column: 0
+                },
+                end: {
+                    line: 1,
+                    column: text.length
+                }
+            });
+            assert.deepEqual(ast.range, [0, text.length]);
+            // should has children
+            assert(ast.children.length > 0);
+        });
     });
-    it("ast.children is Paragraphes", function () {
-        var text = "First Line\n" +
-            "Second Line";
-        var ast = parse(text);
-        // first
-        assert(ast.children[0].type === Syntax.Paragraph);
-        // second
-        assert(ast.children[1].type === Syntax.Paragraph);
-    });
-
     context("Paragraph", function () {
-        it("should be alternately Str and Break", function () {
+        it("should contain Str node", function () {
             var text = "Hello world";
             var ast = parse(text);
             var expected = {
@@ -88,4 +90,347 @@ describe("plaintext-parser-test", function () {
             assert.deepEqual(ast, expected)
         });
     });
+    context("Paragraph ended with break line", function () {
+        it("should contain Break node", function () {
+            var text = "text\n";
+            var ast = parse(text);
+            // Paragraph -> Break
+            var expected = {
+                "type": "Document",
+                "range": [
+                    0,
+                    5
+                ],
+                "loc": {
+                    "start": {
+                        "line": 1,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 1,
+                        "column": 5
+                    }
+                },
+                "children": [
+                    {
+                        "type": "Paragraph",
+                        "raw": "text",
+                        "range": [
+                            0,
+                            4
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 1,
+                                "column": 0
+                            },
+                            "end": {
+                                "line": 1,
+                                "column": 4
+                            }
+                        },
+                        "children": [
+                            {
+                                "type": "Str",
+                                "raw": "text",
+                                "range": [
+                                    0,
+                                    4
+                                ],
+                                "loc": {
+                                    "start": {
+                                        "line": 1,
+                                        "column": 0
+                                    },
+                                    "end": {
+                                        "line": 1,
+                                        "column": 4
+                                    }
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "type": "Break",
+                        "raw": "\n",
+                        "range": [
+                            4,
+                            5
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 1,
+                                "column": 5
+                            },
+                            "end": {
+                                "line": 1,
+                                "column": 5
+                            }
+                        }
+                    }
+                ]
+            };
+            assert.deepEqual(ast, expected)
+        });
+    });
+    context("Paragraph + BR + Paragraph", function () {
+        it("should equal to P + BR + P", function () {
+            var text = "text\ntext";
+            var ast = parse(text);
+            // Paragraph -> Break -> Paragraph
+            var expected = {
+                "type": "Document",
+                "range": [
+                    0,
+                    5
+                ],
+                "loc": {
+                    "start": {
+                        "line": 1,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 1,
+                        "column": 5
+                    }
+                },
+                "children": [
+                    {
+                        "type": "Paragraph",
+                        "raw": "text",
+                        "range": [
+                            0,
+                            4
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 1,
+                                "column": 0
+                            },
+                            "end": {
+                                "line": 1,
+                                "column": 4
+                            }
+                        },
+                        "children": [
+                            {
+                                "type": "Str",
+                                "raw": "text",
+                                "range": [
+                                    0,
+                                    4
+                                ],
+                                "loc": {
+                                    "start": {
+                                        "line": 1,
+                                        "column": 0
+                                    },
+                                    "end": {
+                                        "line": 1,
+                                        "column": 4
+                                    }
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "type": "Break",
+                        "raw": "\n",
+                        "range": [
+                            4,
+                            5
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 1,
+                                "column": 4
+                            },
+                            "end": {
+                                "line": 1,
+                                "column": 5
+                            }
+                        }
+                    },
+                    {
+                        "type": "Paragraph",
+                        "raw": "text",
+                        "range": [
+                            5,
+                            9
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 2,
+                                "column": 0
+                            },
+                            "end": {
+                                "line": 2,
+                                "column": 4
+                            }
+                        },
+                        "children": [
+                            {
+                                "type": "Str",
+                                "raw": "text",
+                                "range": [
+                                    5,
+                                    9
+                                ],
+                                "loc": {
+                                    "start": {
+                                        "line": 2,
+                                        "column": 0
+                                    },
+                                    "end": {
+                                        "line": 2,
+                                        "column": 4
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(ast, expected)
+        });
+    });
+    context("Paragraph + BR + BR + Paragraph", function () {
+        it("should equal to P + BR + BR + P", function () {
+            var text = "text\ntext";
+            var ast = parse(text);
+            // Paragraph -> Break -> Paragraph
+            var expected = {
+                "type": "Document",
+                "range": [
+                    0,
+                    10
+                ],
+                "loc": {
+                    "start": {
+                        "line": 1,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": 1,
+                        "column": 10
+                    }
+                },
+                "children": [
+                    {
+                        "type": "Paragraph",
+                        "raw": "text",
+                        "range": [
+                            0,
+                            4
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 1,
+                                "column": 0
+                            },
+                            "end": {
+                                "line": 1,
+                                "column": 4
+                            }
+                        },
+                        "children": [
+                            {
+                                "type": "Str",
+                                "raw": "text",
+                                "range": [
+                                    0,
+                                    4
+                                ],
+                                "loc": {
+                                    "start": {
+                                        "line": 1,
+                                        "column": 0
+                                    },
+                                    "end": {
+                                        "line": 1,
+                                        "column": 4
+                                    }
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "type": "Break",
+                        "raw": "\n",
+                        "range": [
+                            4,
+                            5
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 1,
+                                "column": 4
+                            },
+                            "end": {
+                                "line": 1,
+                                "column": 5
+                            }
+                        }
+                    },
+                    {
+                        "type": "Break",
+                        "raw": "\n",
+                        "range": [
+                            5,
+                            6
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 2,
+                                "column": 0
+                            },
+                            "end": {
+                                "line": 2,
+                                "column": 1
+                            }
+                        }
+                    },
+                    {
+                        "type": "Paragraph",
+                        "raw": "text",
+                        "range": [
+                            6,
+                            10
+                        ],
+                        "loc": {
+                            "start": {
+                                "line": 3,
+                                "column": 0
+                            },
+                            "end": {
+                                "line": 3,
+                                "column": 4
+                            }
+                        },
+                        "children": [
+                            {
+                                "type": "Str",
+                                "raw": "text",
+                                "range": [
+                                    6,
+                                    10
+                                ],
+                                "loc": {
+                                    "start": {
+                                        "line": 3,
+                                        "column": 0
+                                    },
+                                    "end": {
+                                        "line": 3,
+                                        "column": 4
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            };
+            assert.deepEqual(ast, expected)
+        });
+    })
 });
