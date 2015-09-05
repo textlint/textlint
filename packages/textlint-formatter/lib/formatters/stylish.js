5d606f422a6da91119ece801f96410a20a32081c
azu <azuciao@gmail.com>
2015-09-06 02:39:16 +0900

    chore(stylish): update formatter

--- packages/textlint-formatter/lib/formatters/stylish.js
+++ packages/textlint-formatter/lib/formatters/stylish.js
@@ -8,6 +8,20 @@ var chalk = require("chalk"),
     table = require("text-table");
 
 //------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+/**
+ * Given a word and a count, append an s if count is not one.
+ * @param {string} word A word in its singular form.
+ * @param {int} count A number controlling whether word should be pluralized.
+ * @returns {string} The original word with an s on the end if count is not one.
+ */
+function pluralize(word, count) {
+    return (count === 1 ? word : word + "s");
+}
+
+//------------------------------------------------------------------------------
 // Public Interface
 //------------------------------------------------------------------------------
 
@@ -15,6 +29,8 @@ module.exports = function(results) {
 
     var output = "\n",
         total = 0,
+        errors = 0,
+        warnings = 0,
         summaryColor = "yellow";
 
     results.forEach(function(result) {
@@ -28,40 +44,46 @@ module.exports = function(results) {
         output += chalk.underline(result.filePath) + "\n";
 
         output += table(
-            messages.map(function(message) {
-                var messageType;
+                messages.map(function(message) {
+                    var messageType;
 
-                if (message.fatal || message.severity === 2) {
-                    messageType = chalk.red("error");
-                    summaryColor = "red";
-                } else {
-                    messageType = chalk.yellow("warning");
-                }
+                    if (message.fatal || message.severity === 2) {
+                        messageType = chalk.red("error");
+                        summaryColor = "red";
+                        errors++;
+                    } else {
+                        messageType = chalk.yellow("warning");
+                        warnings++;
+                    }
 
-                return [
-                    "",
-                    message.line || 0,
-                    message.column || 0,
-                    messageType,
-                    message.message.replace(/\.$/, ""),
-                    chalk.gray(message.ruleId || "")
-                ];
-            }),
-            {
-                align: ["", "r", "l"],
-                stringLength: function(str) {
-                    return chalk.stripColor(str).length;
+                    return [
+                        "",
+                        message.line || 0,
+                        message.column || 0,
+                        messageType,
+                        message.message.replace(/\.$/, ""),
+                        chalk.gray(message.ruleId || "")
+                    ];
+                }),
+                {
+                    align: ["", "r", "l"],
+                    stringLength: function(str) {
+                        return chalk.stripColor(str).length;
+                    }
                 }
-            }
-        ).split("\n").map(function(el) {
-            return el.replace(/(\d+)\s+(\d+)/, function(m, p1, p2) {
-                return chalk.gray(p1 + ":" + p2);
-            });
-        }).join("\n") + "\n\n";
+            ).split("\n").map(function(el) {
+                    return el.replace(/(\d+)\s+(\d+)/, function(m, p1, p2) {
+                        return chalk.gray(p1 + ":" + p2);
+                    });
+                }).join("\n") + "\n\n";
     });
 
     if (total > 0) {
-        output += chalk[summaryColor].bold("\u2716 " + total + " problem" + (total === 1 ? "" : "s") + "\n");
+        output += chalk[summaryColor].bold([
+            "\u2716 ", total, pluralize(" problem", total),
+            " (", errors, pluralize(" error", errors), ", ",
+            warnings, pluralize(" warning", warnings), ")\n"
+        ].join(""));
     }
 
     return total > 0 ? output : "";
