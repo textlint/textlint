c8c940865cfc7b77e6bf2723047ce74220d78cc9
nakajmg <nakajima.jmg@gmail.com>
2015-09-15 18:32:35 +0900

    add plugin

---
+++ packages/gulp-textlint/index.js
@@ -0,0 +1,31 @@
+var gutil = require('gulp-util');
+var through = require('through2');
+var TextLintEngine = require('textlint').TextLintEngine;
+
+module.exports = function(options) {
+  options = options || {};
+  var textlint = new TextLintEngine(options);  
+  var filePaths = [];
+  
+  return through.obj(function(file, enc, cb) {
+    console.log(file)
+    filePaths.push(file.path);
+    this.push(file);
+    cb();
+  }, function(cb) {
+    var results;
+    try {
+      results = textlint.executeOnFiles(filePaths);
+      console.log(results);
+      if (textlint.isErrorResults(results)) {
+        gutil.log(textlint.formatResults(results));
+      }
+    }
+    catch(e) {
+      this.emit('error', new gutil.PluginError('textlint', 'Lint failed.'));
+      cb();
+    }
+    this.emit("end");
+    cb();
+  });  
+};
