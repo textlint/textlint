030edc20397082a73584fa3801128658b9591a72
azu <azuciao@gmail.com>
2015-10-04 18:58:42 +0900

    test(example): add plugin test to example-test

--- test/example-test.sh
+++ test/example-test.sh
@@ -4,3 +4,4 @@ rootDir=$(git rev-parse --show-toplevel)
 cd ${rootDir}/examples/cli && npm test
 cd ${rootDir}/examples/config-file && npm test
 cd ${rootDir}/examples/rulesdir && npm test
+cd ${rootDir}/examples/plugin && npm test
