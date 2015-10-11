2b0793908531c023ec1b127be0b7dc98179771b2
azu <azuciao@gmail.com>
2015-10-11 15:55:58 +0900

    docs(txtnode): why loc.line start with 1?

--- docs/txtnode.d.ts
+++ docs/txtnode.d.ts
@@ -10,11 +10,13 @@ declare module TxtSyntax {
         children?: TxtNode[];
     }
     interface LineLocation {
-        start: Position
-        end: Position
+        start: Position;
+        end: Position;
     }
     interface Position {
-        line: number
-        column: number
+        line: number; // start with 1
+        column: number;// start with 0
+        // This is for compatibility with JavaScript AST.
+        // https://gist.github.com/azu/8866b2cb9b7a933e01fe
     }
 }
