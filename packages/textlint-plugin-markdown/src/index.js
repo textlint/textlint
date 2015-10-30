88d0ac526f37daa298762c902fa101ffcc380e5f
azu <azuciao@gmail.com>
2015-10-30 09:35:32 +0900

    initial commmit

---
+++ packages/textlint-plugin-markdown/src/index.js
@@ -0,0 +1,5 @@
+// LICENSE : MIT
+"use strict";
+export default {
+    Processor: require("./MarkdownProcessor")
+};
---
+++ packages/textlint-plugin-markdown/test/MarkdownProcessor-test.js
@@ -0,0 +1,31 @@
+// LICENSE : MIT
+"use strict";
+import assert from "power-assert";
+import MarkdownProcessor from "../src/MarkdownProcessor"
+import {TextLintCore} from "textlint";
+import path from "path";
+describe("MarkdownPlugin", function () {
+    let textlint;
+    beforeEach(function () {
+        textlint = new TextLintCore();
+        textlint.addProcessor(MarkdownProcessor);
+        textlint.setupRules({
+            "no-todo": require("textlint-rule-no-todo")
+        });
+    });
+    context("when target file is a HTML", function () {
+        it("should report error", function () {
+            var fixturePath = path.join(__dirname, "/test.md");
+            let results = textlint.lintFile(fixturePath);
+            assert(results.messages.length > 0);
+            assert(results.filePath === fixturePath);
+        });
+    });
+    context("when target is text", function () {
+        it("should report error", function () {
+            let results = textlint.lintText("TODO: this is todo", ".markdown");
+            assert(results.messages.length === 1);
+            assert(results.filePath === "<markdown>");
+        });
+    });
+});
