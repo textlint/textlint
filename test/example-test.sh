862a9b136a4145f295fca0a55f5f46ba4ebe734e
azu <azuciao@gmail.com>
2015-11-17 10:06:50 +0900

    test(example): add HTML plugin example to test

--- test/example-test.sh
+++ test/example-test.sh
@@ -5,3 +5,4 @@ cd ${rootDir}/examples/cli && npm test
 cd ${rootDir}/examples/config-file && npm test
 cd ${rootDir}/examples/rulesdir && npm test
 cd ${rootDir}/examples/plugin && npm test
+cd ${rootDir}/examples/html-plugin && npm test
