777ff6bd4966fe7cf2813d55149af5ca9ac7e8f9
azu <azuciao@gmail.com>
2015-11-19 23:06:12 +0900

    chore: fix typo

--- src/rule/compute-location.js
+++ src/rule/compute-location.js
@@ -12,7 +12,7 @@ export default function computeLocation(node, padding) {
     let column = node.loc.start.column;
     if (padding.line > 0) {
         line += padding.line;
-        // hen report with padding {line, column}, message.column should be 0 + padding.column.
+        // when report with padding {line, column}, message.column should be 0 + padding.column.
         // In other word, padding line > 0 and message.column start with 0.
         if (padding.column) {
             // means 0 + padding column
