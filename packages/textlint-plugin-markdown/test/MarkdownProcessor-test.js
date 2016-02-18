c8b2aed84d783ac2ba141048622a7c87b0ea86a0
azu <azuciao@gmail.com>
2016-02-18 22:01:14 +0900

    fix(npm): update markdown-to-ast@3.2.3

--- packages/textlint-plugin-markdown/test/MarkdownProcessor-test.js
+++ packages/textlint-plugin-markdown/test/MarkdownProcessor-test.js
@@ -16,16 +16,18 @@ describe("MarkdownPlugin", function () {
     context("when target file is a HTML", function () {
         it("should report error", function () {
             var fixturePath = path.join(__dirname, "/test.md");
-            let results = textlint.lintFile(fixturePath);
-            assert(results.messages.length > 0);
-            assert(results.filePath === fixturePath);
+            return textlint.lintFile(fixturePath).then(results => {
+                assert(results.messages.length > 0);
+                assert(results.filePath === fixturePath);
+            });
         });
     });
     context("when target is text", function () {
         it("should report error", function () {
-            let results = textlint.lintText("TODO: this is todo", ".markdown");
-            assert(results.messages.length === 1);
-            assert(results.filePath === "<markdown>");
+            return textlint.lintText("TODO: this is todo", ".markdown").then(results => {
+                assert(results.messages.length === 1);
+                assert(results.filePath === "<markdown>");
+            });
         });
     });
 });
