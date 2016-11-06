261f1fec04f33a1d8d7978c15dc2ce80e1687b95
azu <azuciao@gmail.com>
2016-11-06 18:55:33 +0900

    style(test): fix style

--- packages/textlint-scripts/scripts/test.js
+++ packages/textlint-scripts/scripts/test.js
@@ -6,12 +6,13 @@ var spawn = require("cross-spawn");
 var args = process.argv.slice(2);
 var mocha = path.resolve(process.cwd(), 'node_modules', '.bin', 'mocha');
 // mocha
-var child =spawn(mocha, [
+var child = spawn(mocha, [
     "--compilers", "js:babel-register",
     "--timeout", "5000",
     "--recursive",
     "test/"
 ].concat(args));
+
 child.stderr.on('data', function(data) {
     process.stderr.write(data);
 });
