7b3855a345137745cd90a773a622ed4c42f0e9c5
azu <azu@users.noreply.github.com>
2017-04-12 00:13:08 +0900

    docs(rule): How to implement "after-all" in the rule? (#269)
    * test(textlint): add "after-all" implementation tests
    
    * docs(rule): add `How to implement "after-all" in the rule?`
    
    * chore: add description
    
    * style(docs): fix lint error

---
+++ packages/textlint/test/rule-tips/rule-tips-after-all-test.js
@@ -0,0 +1,22 @@
+// MIT © 2017 azu
+"use strict";
+import assert from "power-assert";
+import { TextLintCore } from "../../src/index";
+import createAfterAllRule from "./fixtures/rules/after-all";
+describe("rule-tips-after-all-test", function() {
+    // Test https://github.com/textlint/textlint/issues/266#issuecomment-293192017
+    it("should after-all is called at end", function() {
+        const textlint = new TextLintCore();
+        const calls = [];
+        const afterAllRule = createAfterAllRule(calls);
+        textlint.setupRules({
+            "after-all": afterAllRule
+        });
+        const text = "test test";
+        return textlint.lintMarkdown(text).then(() => {
+            const [first, afterAll] = calls;
+            assert.equal(first, text);
+            assert.equal(afterAll, "after-all");
+        });
+    });
+});
