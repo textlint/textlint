3363d6e797b8fdd23410a9ec9c09d32d9071a885
azu <azuciao@gmail.com>
2017-04-11 23:41:15 +0900

    test(textlint): add "after-all" implementation tests

---
+++ packages/textlint/test/rule-tips/fixtures/rules/after-all.js
@@ -0,0 +1,36 @@
+// MIT © 2017 azu
+"use strict";
+const callAsync = (text) => {
+    // do something by async
+    return Promise.resolve(text);
+};
+/**
+ * @param {Array} array
+ * @returns {function(*)}
+ */
+module.exports = (array) => {
+    return (context) => {
+        const { Syntax, getSource } = context;
+        const promiseQueue = [];
+        return {
+            [Syntax.Str](node) {
+                const text = getSource(node);
+                // add it to queue
+                promiseQueue.push(callAsync(text));
+            },
+            // call at the end
+            // Syntax.Document <-> Syntax.Document:exit
+            // https://github.com/textlint/textlint/blob/master/docs/rule.md
+            [`${Syntax.Document}:exit`](){
+                // Note: textlint wait for `Promise.all` is resolved.
+                return Promise.all(promiseQueue).then((...responses) => {
+                    const textAll = responses.join("");
+                    array.push(textAll);
+                }).then(() => {
+                    // after-all
+                    array.push("after-all");
+                });
+            }
+        };
+    };
+};
