12e581d38cb488534d0ff6714d94b5f8c240997a
azu <azuciao@gmail.com>
2017-05-05 15:54:11 +0900

    fix(babel): ignore lib directory

--- packages/textlint-plugin-text/test/TextProcessor-test.js
+++ packages/textlint-plugin-text/test/TextProcessor-test.js
@@ -1,7 +1,7 @@
 // LICENSE : MIT
 "use strict";
 import assert from "assert";
-import TextProcessor from "../src/TextProcessor";
+import { TextProcessor } from "../src/TextProcessor";
 import { TextLintCore } from "textlint";
 import path from "path";
 describe("TextProcessor", function() {
@@ -16,16 +16,18 @@ describe("TextProcessor", function() {
     context("when target file is a Text", function() {
         it("should report error", function() {
             const fixturePath = path.join(__dirname, "fixtures/test.txt");
-            const results = textlint.lintFile(fixturePath);
-            assert(results.messages.length > 0);
-            assert(results.filePath === fixturePath);
+            return textlint.lintFile(fixturePath).then(results => {
+                assert(results.messages.length > 0);
+                assert(results.filePath === fixturePath);
+            });
         });
     });
     context("when target is text", function() {
         it("should report error", function() {
-            const results = textlint.lintText("TODO: this is todo", ".txt");
-            assert(results.messages.length === 1);
-            assert(results.filePath === "<text>");
+            return textlint.lintText("TODO: this is todo", ".txt").then(results => {
+                assert(results.messages.length === 1);
+                assert(results.filePath === "<text>");
+            });
         });
     });
 });
