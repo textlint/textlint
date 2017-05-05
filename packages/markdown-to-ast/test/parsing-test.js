238feefa44c612367b3e59eca27345696926f636
azu <azuciao@gmail.com>
2017-05-05 21:59:10 +0900

    fix(markdown-to-ast): fix test case

--- packages/markdown-to-ast/test/parsing-test.js
+++ packages/markdown-to-ast/test/parsing-test.js
@@ -5,6 +5,7 @@ const assert = require("assert");
 const parse = require("../src/markdown-parser").parse;
 const fs = require("fs");
 const path = require("path");
+const isTxtAST = require("textlint-ast-test").isTxtAST;
 describe("parsing", function() {
     const fixtureDir = path.join(__dirname, "fixtures");
     fs.readdirSync(fixtureDir).forEach(function(filePath) {
@@ -12,6 +13,7 @@ describe("parsing", function() {
         it(dirName + " match AST", function() {
             const input = fs.readFileSync(path.join(fixtureDir, filePath, "input.md"), "utf-8");
             const AST = parse(input);
+            assert(isTxtAST(AST), "AST Should be valid AST");
             const output = JSON.parse(fs.readFileSync(path.join(fixtureDir, filePath, "output.json"), "utf-8"));
             assert.deepEqual(AST, output);
         });
