197d5d4426a7eb44cd73ae99ad046090ffeaccdb
azu <azu@users.noreply.github.com>
2017-05-05 22:16:30 +0900

    feat(markdown-to-ast): update remark@7 (#278)
    BREAKING CHANGE: some parse result is changed.
    
    It is correct, but we think that is a breacking change.
    This change have possibilities that the user update patch-version textlint and fail test.
    
    close https://github.com/textlint/markdown-to-ast/issues/14

--- packages/markdown-to-ast/test/compliance-tests.js
+++ packages/markdown-to-ast/test/compliance-tests.js
@@ -3,20 +3,12 @@
 const assert = require("assert");
 const test = require("textlint-ast-test").test;
 const parse = require("../src/markdown-parser").parse;
-const fs = require("fs");
-const path = require("path");
 // String -> [String]
-function fileList(dir) {
-    return fs.readdirSync(dir).reduce(function (list, file) {
-        var name = path.join(dir, file);
-        var isDir = fs.statSync(name).isDirectory();
-        return list.concat(isDir ? fileList(name) : [name]);
-    }, []);
-}
 describe("Compliance tests", function () {
     context("compatible for Unist", function () {
         it("should have position", function () {
-            var AST = parse("text");
+            const AST = parse("text");
+            test(AST);
             assert(typeof AST.position === "object");
             assert(typeof AST.position.start === "object");
             assert(typeof AST.position.start.line === "number");
@@ -24,17 +16,6 @@ describe("Compliance tests", function () {
             assert(typeof AST.position.end === "object");
             assert(typeof AST.position.end.line === "number");
             assert(typeof AST.position.end.column === "number");
-        });
-    });
-    context("when use fixtures", function () {
-        var fixtureFiles = fileList(path.join(__dirname, "fixtures"));
-        fixtureFiles.forEach(function (filePath) {
-            var fileName = path.basename(filePath);
-            it("should pass " + fileName + " and return AST has type", function () {
-                var content = fs.readFileSync(filePath, "utf-8");
-                var AST = parse(content);
-                test(AST);
-            });
         });
     });
 });
---
+++ packages/markdown-to-ast/test/fixtures/list-item-second-paragraph/input.md
@@ -0,0 +1,6 @@
+- Paragraph is a `TxtParentNode` and type is Paragraph
+    - have `children`, but not have `value`
+*Emphasis* is a `TxtTextNode` and type is Emphasis
+    - have `value`
+"text" is a `TxtTextNode` and type is Str
+    - have `value`
