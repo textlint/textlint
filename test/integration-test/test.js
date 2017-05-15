603d43a70b987b5c56d8645cf4d9b3f92db9fe14
azu <azuciao@gmail.com>
2017-05-15 22:31:34 +0900

    test(integration): version specific

--- test/integration-test/test.js
+++ test/integration-test/test.js
@@ -1,3 +1,4 @@
+const semver = require("semver");
 const runLint = require("./run_lint");
 const testList = [
     {
@@ -9,7 +10,8 @@ const testList = [
     },
     {
         name: "webpack-book",
-        path: "manuscript/"
+        path: "manuscript/",
+        version: "6.0.0"
     },
     {
         name: "scala_text",
@@ -19,7 +21,15 @@ const testList = [
         name: "magi-hacker",
         path: "chapter-01/ chapter-02/ chapter-03/ chapter-04/ chapter-05/"
     }
-];
+].filter(testTarget => {
+    const version = process.version;
+    if(testTarget.version === undefined) {
+        return true;
+    }
+    return semver.gte(version, testTarget.version);
+});
+
+
 
 testList.forEach((test) => {
     runLint(test.name, test.path);
