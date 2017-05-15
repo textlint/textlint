a29e04434771a3f5be2cffa888faa601beda1a68
azu <azuciao@gmail.com>
2017-05-15 10:14:52 +0900

    refactor(test): test.js define test list as object

--- test/integration-test/test.js
+++ test/integration-test/test.js
@@ -1,6 +1,26 @@
 const runLint = require("./run_lint");
-runLint("webpack-book", "manuscript/");
-runLint("JavaScript-Plugin-Architecture", "ja/");
-runLint("gitbook-starter-kit", "ja/");
-runLint("scala_text", "src/");
-runLint("magi-hacker", "chapter-01/ chapter-02/ chapter-03/ chapter-04/ chapter-05/");
+const testList = [
+    {
+        name: "webpack-book",
+        path: "manuscript/"
+    },
+    {
+        name: "JavaScript-Plugin-Architecture",
+        path: "ja/"
+    }, {
+        name: "gitbook-starter-kit",
+        path: "ja/"
+    },
+    {
+        name: "scala_text",
+        path: "src/"
+    },
+    {
+        name: "magi-hacker",
+        path: "chapter-01/ chapter-02/ chapter-03/ chapter-04/ chapter-05/"
+    }
+];
+
+testList.forEach((test) => {
+    runLint(test.name, test.path);
+});
