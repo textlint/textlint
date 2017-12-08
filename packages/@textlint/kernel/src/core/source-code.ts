7ca9b443b51ee24c77f417a0602eed8b06cbaff4
azu <azuciao@gmail.com>
2017-12-08 11:37:31 +0900

    fix JSDOc

--- packages/@textlint/kernel/src/core/source-code.ts
+++ packages/@textlint/kernel/src/core/source-code.ts
@@ -6,7 +6,7 @@ import { ASTNodeTypes } from "@textlint/ast-node-types";
 
 /**
  * Validates that the given AST has the required information.
- * @param {TxtAST.TxtNode} [ast] The Program node of the AST to check.
+ * @param {TxtNode} [ast] The Program node of the AST to check.
  * @throws {Error} If the AST doesn't contain the correct information.
  * @returns {void}
  * @private
