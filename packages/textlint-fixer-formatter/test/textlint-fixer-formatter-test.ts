47e56704c388349345e5bd6b0f140d5481210e0c
Kaoru <679719+0x6b@users.noreply.github.com>
2017-12-23 17:22:09 +0900

    test(textlint-fixer-formatter): convert new test case to TypeScript

--- packages/textlint-fixer-formatter/test/textlint-fixer-formatter-test.ts
+++ packages/textlint-fixer-formatter/test/textlint-fixer-formatter-test.ts
@@ -1,14 +1,13 @@
 // LICENSE : MIT
-'use strict'
-var path   = require('path')
-var assert = require('power-assert')
-import { getFormatterList } from 'textlint-fixer-formatter'
+"use strict";
+import { getFormatterList } from "textlint-fixer-formatter"
+import assert from "power-assert";
 
 describe('textlint-fixer-formatter-test', function () {
   describe('getFormatterList', function () {
     it('should return list of formatter(s)', function () {
-      assert(getFormatterList(), [
-          {name: 'compact'},
+      assert.deepEqual(getFormatterList(), [
+          {name: 'compats'},
           {name: 'diff'},
           {name: 'json'},
           {name: 'stylish'}
@@ -16,4 +15,4 @@ describe('textlint-fixer-formatter-test', function () {
       )
     })
   })
-})
+});
