b5ea386c0a334d61397d628b5d6c2a2e07abcab6
Kaoru <679719+0x6b@users.noreply.github.com>
2018-01-13 16:51:43 +0900

    refactor(docs): initialize Docusaurus

---
+++ website/core/Footer.js
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require("react");
+
+class Footer extends React.Component {
+    docUrl(doc, language) {
+        const baseUrl = this.props.config.baseUrl;
+        return baseUrl + "docs/" + (language ? language + "/" : "") + doc;
+    }
+
+    pageUrl(doc, language) {
+        const baseUrl = this.props.config.baseUrl;
+        return baseUrl + (language ? language + "/" : "") + doc;
+    }
+
+    render() {
+        const currentYear = new Date().getFullYear();
+        return (
+            <footer className="nav-footer" id="footer">
+                <section className="sitemap">
+                    <a href={this.props.config.baseUrl} className="nav-home">
+                        {this.props.config.footerIcon && (
+                            <img
+                                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                                alt={this.props.config.title}
+                                width="66"
+                                height="58"
+                            />
+                        )}
+                    </a>
+                    <div>
+                        <h5>Docs</h5>
+                        <a href={this.docUrl("getting-started.html", this.props.language)}>Getting Started</a>
+                        <a href={this.docUrl("configuring.html", this.props.language)}>User manual</a>
+                        <a href={this.docUrl("rule.html", this.props.language)}>Developer documentation</a>
+                    </div>
+                    <div>
+                        <h5>Community</h5>
+                        <a href="https://gitter.im/textlint-ja/textlint-ja">Project Chat</a>
+                    </div>
+                    <div>
+                        <h5>More</h5>
+                        <a href={this.props.config.baseUrl + "blog"}>Blog</a>
+                        <a href="https://github.com/">GitHub</a>
+                        <a
+                            className="github-button"
+                            href={this.props.config.repoUrl}
+                            data-icon="octicon-star"
+                            data-count-href="/facebook/docusaurus/stargazers"
+                            data-show-count={true}
+                            data-count-aria-label="# stargazers on GitHub"
+                            aria-label="Star this project on GitHub"
+                        >
+                            Star
+                        </a>
+                    </div>
+                </section>
+
+                <section className="copyright">Copyright &copy; {currentYear} textlint organization</section>
+            </footer>
+        );
+    }
+}
+
+module.exports = Footer;
