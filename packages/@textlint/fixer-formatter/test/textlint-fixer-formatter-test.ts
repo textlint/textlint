9aa6dbe8dee980e8d966178b249fdb37bd9a00c8
Kaoru <679719+0x6b@users.noreply.github.com>
2018-01-18 21:45:56 +0900

    refactor(fixer-formatter): update usage of @textlint/fixer-formatter

--- packages/@textlint/fixer-formatter/test/textlint-fixer-formatter-test.ts
+++ packages/@textlint/fixer-formatter/test/textlint-fixer-formatter-test.ts
@@ -3,7 +3,7 @@
 import { getFixerFormatterList } from "@textlint/fixer-formatter";
 import assert from "power-assert";
 
-describe("textlint-fixer-formatter-test", function() {
+describe("@textlint/fixer-formatter-test", function() {
     describe("getFormatterList", function() {
         it("should return list of formatter(s)", function() {
             assert.deepEqual(getFixerFormatterList(), [
