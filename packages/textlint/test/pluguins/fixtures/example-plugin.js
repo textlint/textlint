bbfc8f6d6faed489a4d5b8fc5713d9c511688637
azu <azuciao@gmail.com>
2018-07-16 10:31:12 +0900

    test(textlint): fix plugin tests

--- packages/textlint/test/pluguins/fixtures/example-plugin.js
+++ packages/textlint/test/pluguins/fixtures/example-plugin.js
@@ -43,7 +43,7 @@ export const createPluginStub = () => {
         getOptions() {
             return assignedOptions;
         },
-        createPlugin() {
+        get plugin() {
             return {
                 Processor: class MockProcessor extends ExampleProcessor {
                     constructor(options) {
