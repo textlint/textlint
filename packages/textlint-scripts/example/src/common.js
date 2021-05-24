90a564f2872be887a8373647eed084b6c46b50c7
azu <azuciao@gmail.com>
2021-05-24 21:17:35 +0900

    fix(textlint-scripts): fix example code

--- packages/textlint-scripts/example/src/common.js
+++ packages/textlint-scripts/example/src/common.js
@@ -1,8 +1,8 @@
 import path from "path";
-import prh from "prh";
+import { fromYAML } from "prh";
 import fs from "fs";
 export default function (text) {
     const dict = fs.readFileSync(path.join(__dirname, "prh.yml"), "utf-8");
-    const engine = prh.fromYAML("", dict);
+    const engine = fromYAML("", dict);
     return engine.makeChangeSet("", text);
 }
