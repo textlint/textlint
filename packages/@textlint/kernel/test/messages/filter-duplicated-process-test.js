7ccbf76a336eebda28d1396c9ca2443ae25f2dc3
azu <azuciao@gmail.com>
2021-12-25 23:55:41 +0900

    fix: isEqualMessage use `range`

--- packages/@textlint/kernel/test/messages/filter-duplicated-process-test.js
+++ packages/@textlint/kernel/test/messages/filter-duplicated-process-test.js
@@ -2,6 +2,7 @@
 "use strict";
 import filterMessages from "../../src/messages/filter-duplicated-process";
 import * as assert from "assert";
+
 describe("message-filter", function () {
     context("when pass empty messages", function () {
         it("should return empty messages", function () {
@@ -11,7 +12,26 @@ describe("message-filter", function () {
     context("when only lint messages", function () {
         it("should not change messages", function () {
             const messages = [
-                { type: "lint", ruleId: "rule", message: "message", index: 1, line: 1, column: 2, severity: 2 }
+                {
+                    type: "lint",
+                    ruleId: "rule",
+                    message: "message",
+                    index: 1,
+                    line: 1,
+                    column: 2,
+                    range: [0, 1],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 2
+                        },
+                        end: {
+                            line: 1,
+                            column: 3
+                        }
+                    },
+                    severity: 2
+                }
             ];
             assert.equal(filterMessages(messages).length, 1);
             assert.deepEqual(filterMessages(messages), messages);
@@ -20,16 +40,111 @@ describe("message-filter", function () {
     context("when contain duplicated messages", function () {
         it("should filter to be one", function () {
             const messages = [
-                { type: "lint", ruleId: "rule", message: "message", index: 0, line: 1, column: 2, severity: 2 },
-                { type: "lint", ruleId: "rule", message: "message", index: 0, line: 1, column: 2, severity: 2 }
+                {
+                    type: "lint",
+                    ruleId: "rule",
+                    message: "message",
+                    index: 0,
+                    line: 1,
+                    column: 2,
+                    range: [0, 1],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 2
+                        },
+                        end: {
+                            line: 1,
+                            column: 3
+                        }
+                    },
+                    severity: 2
+                },
+                {
+                    type: "lint",
+                    ruleId: "rule",
+                    message: "message",
+                    index: 0,
+                    line: 1,
+                    column: 2,
+                    range: [0, 1],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 2
+                        },
+                        end: {
+                            line: 1,
+                            column: 3
+                        }
+                    },
+                    severity: 2
+                }
             ];
             assert.equal(filterMessages(messages).length, 1);
         });
         it("should filter 3 -> 1", function () {
             const messages = [
-                { type: "lint", ruleId: "rule", message: "message", index: 0, line: 1, column: 1, severity: 2 },
-                { type: "lint", ruleId: "rule", message: "message", index: 0, line: 1, column: 1, severity: 2 },
-                { type: "lint", ruleId: "rule", message: "message", index: 0, line: 1, column: 1, severity: 2 }
+                {
+                    type: "lint",
+                    ruleId: "rule",
+                    message: "message",
+                    index: 0,
+                    line: 1,
+                    column: 1,
+                    range: [0, 1],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 2
+                        },
+                        end: {
+                            line: 1,
+                            column: 3
+                        }
+                    },
+                    severity: 2
+                },
+                {
+                    type: "lint",
+                    ruleId: "rule",
+                    message: "message",
+                    index: 0,
+                    line: 1,
+                    column: 1,
+                    range: [0, 1],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 2
+                        },
+                        end: {
+                            line: 1,
+                            column: 3
+                        }
+                    },
+                    severity: 2
+                },
+                {
+                    type: "lint",
+                    ruleId: "rule",
+                    message: "message",
+                    index: 0,
+                    line: 1,
+                    column: 1,
+                    range: [0, 1],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 2
+                        },
+                        end: {
+                            line: 1,
+                            column: 3
+                        }
+                    },
+                    severity: 2
+                }
             ];
             assert.equal(filterMessages(messages).length, 1);
         });
@@ -37,8 +152,46 @@ describe("message-filter", function () {
     context("when duplicated message, but ruleId is difference", function () {
         it("should filter messages", function () {
             const messages = [
-                { type: "lint", ruleId: "rule-x", message: "message", index: 10, line: 1, column: 4, severity: 2 },
-                { type: "lint", ruleId: "rule-y", message: "message", index: 10, line: 1, column: 4, severity: 2 }
+                {
+                    type: "lint",
+                    ruleId: "rule-x",
+                    message: "message",
+                    index: 10,
+                    line: 1,
+                    column: 4,
+                    range: [10, 11],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 10
+                        },
+                        end: {
+                            line: 1,
+                            column: 11
+                        }
+                    },
+                    severity: 2
+                },
+                {
+                    type: "lint",
+                    ruleId: "rule-y",
+                    message: "message",
+                    index: 10,
+                    line: 1,
+                    column: 4,
+                    range: [10, 11],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 10
+                        },
+                        end: {
+                            line: 1,
+                            column: 11
+                        }
+                    },
+                    severity: 2
+                }
             ];
             assert.equal(filterMessages(messages).length, 1);
         });
@@ -46,8 +199,46 @@ describe("message-filter", function () {
     context("when duplicated message, but message is difference", function () {
         it("should not filter messages", function () {
             const messages = [
-                { type: "lint", ruleId: "rule-x", message: "x is x", index: 10, line: 1, column: 4, severity: 2 },
-                { type: "lint", ruleId: "rule-y", message: "y is y", index: 10, line: 1, column: 4, severity: 2 }
+                {
+                    type: "lint",
+                    ruleId: "rule-x",
+                    message: "x is x",
+                    index: 10,
+                    line: 1,
+                    column: 4,
+                    range: [10, 11],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 10
+                        },
+                        end: {
+                            line: 1,
+                            column: 11
+                        }
+                    },
+                    severity: 2
+                },
+                {
+                    type: "lint",
+                    ruleId: "rule-y",
+                    message: "y is y",
+                    index: 10,
+                    line: 1,
+                    column: 4,
+                    range: [10, 11],
+                    loc: {
+                        start: {
+                            line: 1,
+                            column: 10
+                        },
+                        end: {
+                            line: 1,
+                            column: 11
+                        }
+                    },
+                    severity: 2
+                }
             ];
             assert.equal(filterMessages(messages).length, 2);
         });
