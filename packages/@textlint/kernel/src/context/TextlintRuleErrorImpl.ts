8c1fc23f2a78cd96268b5b28d49783d1f6126748
azu <azu@users.noreply.github.com>
2022-06-25 16:36:49 +0900

    fix(textlint/kernel): improve RuleError error handlong (#877)

--- packages/@textlint/kernel/src/context/TextlintRuleErrorImpl.ts
+++ packages/@textlint/kernel/src/context/TextlintRuleErrorImpl.ts
@@ -3,6 +3,67 @@
 
 import type { TextlintRuleContextFixCommand, TextlintRuleErrorDetails, TextlintRuleError } from "@textlint/types";
 import { TextlintRuleErrorPaddingLocation } from "@textlint/types";
+import { throwIfTesting } from "@textlint/feature-flag";
+import { isTextlintRuleErrorPaddingLocation } from "./TextlintRulePaddingLocator";
+
+const assertTextlintRuleErrorDetail = (details: TextlintRuleErrorDetails) => {
+    // reject wrong usage
+    if ("at" in details) {
+        throw new Error(
+            `RuleError details should not have "loc" property. Please use { padding: locator.at(index) } instead.`
+        );
+    }
+    if ("range" in details) {
+        throw new Error(
+            `RuleError details should not have "range" property. Please use { padding: locator.range([start, end]) } instead.`
+        );
+    }
+    if ("loc" in details) {
+        throw new Error(
+            `RuleError details should not have "loc" property. Please use { padding: locator.loc({ start, end }) } instead.`
+        );
+    }
+    // wrong mixed usage
+    const useIndex = "index" in details;
+    const useLineColumn = "line" in details || "column" in details;
+    const usePadding = "padding" in details;
+    if ([useIndex, useLineColumn, usePadding].filter(Boolean).length > 1) {
+        throwIfTesting(`RuleError details can not mixed usage: ${JSON.stringify(details)}
+
+You have set { index, line, column, padding } at same time.        
+`);
+    }
+    // legacy usage
+    if (useIndex) {
+        if (typeof details.index !== "number" || Number.isNaN(details.index)) {
+            throwIfTesting(`index should be number: ${JSON.stringify(details)}`);
+        }
+        return;
+    }
+
+    if (useLineColumn) {
+        // TODO: make throw error in next version
+        const shouldHaveLineAndColumn = "line" in details && "column" in details;
+        if (!shouldHaveLineAndColumn) {
+            throwIfTesting("line and column should be set both");
+        }
+        if (typeof details.line !== "number" || Number.isNaN(details.line)) {
+            throwIfTesting(`line should be number: ${JSON.stringify(details)}`);
+        }
+        if (typeof details.column !== "number" || Number.isNaN(details.column)) {
+            throwIfTesting(`column should be number: ${JSON.stringify(details)}`);
+        }
+        return;
+    }
+    // modern usage
+    if (usePadding) {
+        // padding assertion is done in locator function
+        if (!isTextlintRuleErrorPaddingLocation(details.padding)) {
+            throwIfTesting(`padding should be created locator function: ${JSON.stringify(details)}`);
+        }
+        return;
+    }
+};
 
 export class TextlintRuleErrorImpl implements TextlintRuleError {
     public message: string;
@@ -35,6 +96,7 @@ export class TextlintRuleErrorImpl implements TextlintRuleError {
     constructor(message: string, details?: number | TextlintRuleErrorDetails) {
         this.message = message;
         if (typeof details === "object") {
+            assertTextlintRuleErrorDetail(details);
             /**
              * padding lineNumber
              * @type {number}
