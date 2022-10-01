748d69d40746f5e8889fa2a9b8dca547a45ddfc0
Fatih Altinok <fatihaltinok@live.com>
2022-10-01 16:23:21 +0300

    Fix Github link in footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -45,7 +45,7 @@ class Footer extends React.Component {
                     <div>
                         <h5>More</h5>
                         <a href={this.props.config.baseUrl + "blog"}>Blog</a>
-                        <a href="https://github.com/">GitHub</a>
+                        <a href="https://github.com/textlint/textlint">GitHub</a>
                         <a
                             className="github-button"
                             href={this.props.config.repoUrl}
