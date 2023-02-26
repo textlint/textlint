487ff230e4e16c9268e5fea1730e25fb731f2c70
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-02-26 14:28:36 +0900

    Fix test title

--- packages/textlint/test/loadTextlintrc/loadTextlintrc.test.ts
+++ packages/textlint/test/loadTextlintrc/loadTextlintrc.test.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { loadTextlintrc } from "../../src";
 
 describe("loadTextlintrc", () => {
-    it("should not throw TypeError when parameter is empty", async () => {
+    it("should not throw TypeError when parameters are empty", async () => {
         assert.doesNotThrow(async () => {
             await loadTextlintrc();
         }, TypeError);
