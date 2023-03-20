f205122dfdf0d9b8c203da640f9c3425f42d34fa
github-action-benchmark <github@users.noreply.github.com>
2023-03-20 05:50:46 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for bbea40ed84295973f4ae1d6c1077e7d3289b7b1f

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679068040140,
+  "lastUpdate": 1679291446911,
   "repoUrl": "https://github.com/textlint/textlint",
   "entries": {
     "Benchmark": [
@@ -4494,6 +4494,48 @@ window.BENCHMARK_DATA = {
             "value": 2.2604808235599996,
             "unit": "seconds",
             "range": "± 0.057038481999999835"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bbea40ed84295973f4ae1d6c1077e7d3289b7b1f",
+          "message": "chore(deps): update dependency @types/node to ^18.15.1",
+          "timestamp": "2023-03-20T05:47:12Z",
+          "tree_id": "733122e83f5126c7d10e8c8b5f5c8fc5c46de6c1",
+          "url": "https://github.com/textlint/textlint/commit/bbea40ed84295973f4ae1d6c1077e7d3289b7b1f"
+        },
+        "date": 1679291445737,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "npm run bench:self",
+            "value": 4.0452032017,
+            "unit": "seconds",
+            "range": "± 0.1276346239999997"
+          },
+          {
+            "name": "npm run bench:technical-writing",
+            "value": 12.157238984900001,
+            "unit": "seconds",
+            "range": "± 0.178375771999999"
+          },
+          {
+            "name": "npm run bench:jtf-style",
+            "value": 1.8275742835000002,
+            "unit": "seconds",
+            "range": "± 0.027498626999999942"
           }
         ]
       }
