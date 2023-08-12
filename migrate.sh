eff09d1524163db28b469958095726bba9cdf806
azu <azuciao@gmail.com>
2023-08-12 21:22:00 +0900

    chore: remove migrate script

--- migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-npm install -g jscodeshift
-LATEST_VERSION=$(npm view commonjs-to-es-module-codemod version)
-jscodeshift -t "https://unpkg.com/commonjs-to-es-module-codemod@${LATEST_VERSION}/dist/index.js" --parser ts --extensions ts \
-./packages/@textlint/**/src/**/*.ts
