2722909fb990c38407ca3af8dfea50f07a5dcf5e
Noritaka Kobayashi <arukimedesu1166777@gmail.com>
2025-05-26 22:50:19 +0900

    refactor: remove unused imports (#1515)
    * refactor: remove unused imports & dependencies
    
    * fix: revert remove package

--- website/src/pages/users.jsx
+++ website/src/pages/users.jsx
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./users.module.css";
