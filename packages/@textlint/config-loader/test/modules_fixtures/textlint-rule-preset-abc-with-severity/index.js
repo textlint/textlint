2a1a50d6b4954552e9059ad0b15d2f35db24313b
azu <azuciao@gmail.com>
2025-06-29 21:41:40 +0900

    tett(config-loader): add user config is `true` case

--- packages/@textlint/config-loader/test/modules_fixtures/textlint-rule-preset-abc-with-severity/index.js
+++ packages/@textlint/config-loader/test/modules_fixtures/textlint-rule-preset-abc-with-severity/index.js
@@ -2,11 +2,13 @@ module.exports = {
     rules: {
         a: require("../textlint-rule-a"),
         b: require("../textlint-rule-b"),
-        c: require("../textlint-rule-c-sub1")
+        c1: require("../textlint-rule-c-sub1"),
+        c2: require("../textlint-rule-c-sub2")
     },
     rulesConfig: {
         a: { severity: "warning", presetOption: "default" },
         b: { severity: "warning", anotherPresetOption: "value" },
-        c: { severity: "warning" }
+        c1: { severity: "warning" },
+        c2: { severity: "warning" }
     }
 };
