74f0e73e740599432b5f97fe1a4de3123820fb09
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2026-05-13 22:56:22 +0900

    feat(ast-node-types): make TxtNode properties readonly

--- packages/@textlint/types/test/Rule/TxtNode.test.ts
+++ packages/@textlint/types/test/Rule/TxtNode.test.ts
@@ -33,6 +33,31 @@ const report: TextlintRuleReporter = (context) => {
     return {
         [Syntax.Document](node) {
             expectType<TxtDocumentNode>(node);
+            // @ts-expect-error TxtNode should be readonly
+            node.type = "Paragraph";
+            // @ts-expect-error TxtNode should be readonly
+            node.raw = "";
+            // @ts-expect-error TxtNode should be readonly
+            node.range = [0, 0];
+            // @ts-expect-error TxtNode should be readonly
+            node.loc = {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 1 },
+            };
+            // @ts-expect-error TxtNode children should be readonly
+            node.children = [];
+            // @ts-expect-error TxtNode children should be readonly
+            node.children.push(node.children[0]);
+            // @ts-expect-error TxtNode location should be readonly
+            node.loc.start = { line: 1, column: 0 };
+            // @ts-expect-error TxtNode location should be readonly
+            node.loc.end = { line: 1, column: 1 };
+            // @ts-expect-error TxtNode location should be readonly
+            node.loc.start.line = 1;
+            // @ts-expect-error TxtNode location should be readonly
+            node.loc.start.column = 0;
+            // @ts-expect-error TxtNode parent should be readonly
+            node.parent = node;
         },
         [Syntax.DocumentExit](node) {
             expectType<TxtDocumentNode>(node);
@@ -51,36 +76,60 @@ const report: TextlintRuleReporter = (context) => {
         },
         [Syntax.List](node) {
             expectType<TxtListNode>(node);
+            // @ts-expect-error TxtListNode ordered should be readonly
+            node.ordered = true;
+            // @ts-expect-error TxtListNode start should be readonly
+            node.start = 1;
+            // @ts-expect-error TxtListNode spread should be readonly
+            node.spread = false;
         },
         [Syntax.ListExit](node) {
             expectType<TxtListNode>(node);
         },
         [Syntax.ListItem](node) {
             expectType<TxtListItemNode>(node);
+            // @ts-expect-error TxtListItemNode checked should be readonly
+            node.checked = false;
         },
         [Syntax.ListItemExit](node) {
             expectType<TxtListItemNode>(node);
         },
         [Syntax.Header](node) {
             expectType<TxtHeaderNode>(node);
+            // @ts-expect-error TxtHeaderNode depth should be readonly
+            node.depth = 2;
         },
         [Syntax.HeaderExit](node) {
             expectType<TxtHeaderNode>(node);
         },
         [Syntax.CodeBlock](node) {
             expectType<TxtCodeBlockNode>(node);
+            // @ts-expect-error TxtCodeBlockNode lang should be readonly
+            node.lang = "js";
+            // @ts-expect-error TxtCodeBlockNode meta should be readonly
+            node.meta = "meta";
         },
         [Syntax.CodeBlockExit](node) {
             expectType<TxtCodeBlockNode>(node);
         },
         [Syntax.Link](node) {
             expectType<TxtLinkNode>(node);
+            // @ts-expect-error TxtResource url should be readonly
+            node.url = "https://example.com";
+            // @ts-expect-error TxtResource title should be readonly
+            node.title = "example";
         },
         [Syntax.LinkExit](node) {
             expectType<TxtLinkNode>(node);
         },
         [Syntax.LinkReference](node) {
             expectType<TxtLinkReferenceNode>(node);
+            // @ts-expect-error TxtLinkReferenceNode referenceType should be readonly
+            node.referenceType = "full";
+            // @ts-expect-error TxtReference identifier should be readonly
+            node.identifier = "id";
+            // @ts-expect-error TxtReference label should be readonly
+            node.label = "label";
         },
         [Syntax.LinkReferenceExit](node) {
             expectType<TxtLinkReferenceNode>(node);
@@ -111,6 +160,8 @@ const report: TextlintRuleReporter = (context) => {
         },
         [Syntax.Image](node) {
             expectType<TxtImageNode>(node);
+            // @ts-expect-error TxtAlternative alt should be readonly
+            node.alt = "alt";
         },
         [Syntax.ImageExit](node) {
             expectType<TxtImageNode>(node);
@@ -141,6 +192,8 @@ const report: TextlintRuleReporter = (context) => {
         },
         [Syntax.Str](node) {
             expectType<TxtStrNode>(node);
+            // @ts-expect-error TxtTextNode value should be readonly
+            node.value = "modified";
         },
         [Syntax.StrExit](node) {
             expectType<TxtStrNode>(node);
@@ -159,6 +212,8 @@ const report: TextlintRuleReporter = (context) => {
         },
         [Syntax.Table](node) {
             expectType<TxtTableNode>(node);
+            // @ts-expect-error TxtTableNode align should be readonly
+            node.align?.push("left");
         },
         [Syntax.TableExit](node) {
             expectType<TxtTableNode>(node);
