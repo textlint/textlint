121e3b11c94cadcd0a09960e873750dd649c30f2
Mustafa Senoglu <mustafa.senoglu@example.com>
2026-07-01 14:17:45 +0300

    fix: prevent duplicated register in textlint-scripts
    Fixes #781

--- packages/textlint-scripts/register-ts.js
+++ packages/textlint-scripts/register-ts.js
@@ -1,2 +1,6 @@
 // --require textlint-scripts/register-ts
-require("./configs/ts-node-register");
+// Prevent duplicate registration
+if (!global.__textlint_scripts_register_ts_loaded) {
+    global.__textlint_scripts_register_ts_loaded = true;
+    require("./configs/ts-node-register");
+}
